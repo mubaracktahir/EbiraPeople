dc8b6242b0e305fc16ed418e3e28c1a543db5108
Mubarack tahir <mubarack.tahir@yahoo.com>
2020-07-30 12:33:42 +0100

    created the necessary packages for the app (subject to change)

---
+++ app/src/main/java/com/mubaracktahir/ebirapeople/EbiraPeople.kt
@@ -0,0 +1,15 @@
+package com.mubaracktahir.ebirapeople
+
+import android.app.Application
+
+
+/**
+ * Created by Mubarak Tahir on 7/30/2020.
+ * Mubby inc
+ * mubarack.tahirr@gmail.com
+ */
+class EbiraPeople : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/mubaracktahir/ebirapeople/UI/Main/MainActivity.kt
+++ app/src/main/java/com/mubaracktahir/ebirapeople/UI/Main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mubaracktahir.ebirapeople
+package com.mubaracktahir.ebirapeople.UI.Main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mubaracktahir.ebirapeople.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
