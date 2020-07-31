a1c15f5066ccb90e26932800cadfd894c4a860db
Mubarack tahir <mubarack.tahir@yahoo.com>
2020-07-31 19:20:08 +0100

    added navigation.xml,SplashScreenFragment.

---
+++ app/src/main/java/com/mubaracktahir/ebirapeople/UI/SplashScreen/SplashScreenFragment.kt
@@ -0,0 +1,20 @@
+package com.mubaracktahir.ebirapeople.UI.SplashScreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mubaracktahir.ebirapeople.R
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+}
