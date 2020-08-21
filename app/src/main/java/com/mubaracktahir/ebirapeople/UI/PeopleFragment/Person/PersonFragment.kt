d0ab8f8c88c45e4a9821515a45e2758cdc73f299
Mubarack tahir <mubarack.tahir@yahoo.com>
2020-08-21 14:59:45 +0100

    added styles

--- app/src/main/java/com/mubaracktahir/ebirapeople/UI/PeopleFragment/Person/PersonFragment.kt
+++ app/src/main/java/com/mubaracktahir/ebirapeople/UI/PeopleFragment/Person/PersonFragment.kt
@@ -7,15 +7,12 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.mubaracktahir.ebirapeople.R
 import com.mubaracktahir.ebirapeople.core.BaseFragment
 import com.mubaracktahir.ebirapeople.core.FullScreenFragment
 import com.mubaracktahir.ebirapeople.databinding.FragmentPersonBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [FullScreenFragment] subclass.
@@ -28,6 +25,10 @@ class PersonFragment : FullScreenFragment<FragmentPersonBinding>( R.layout.fragm
     override fun init() {
         val args = PersonFragmentArgs.fromBundle(requireArguments())
         val person = args.person
+        binding.fabs.setOnClickListener {
+            //activity?.supportFragmentManager?.popBackStack()
+            view?.findNavController()?.popBackStack()
+        }
         binding.person = person
         binding.executePendingBindings()
     }
