ae9a95df00a1a360b6c996a369a36a28e2542563
Mubarack tahir <mubarack.tahir@yahoo.com>
2020-08-22 14:10:08 +0100

    added auto scrolling

---
+++ app/src/main/java/com/mubaracktahir/ebirapeople/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.mubaracktahir.ebirapeople.utils
+
+
+/**
+ * Created by Mubarak Tahir on 8/21/2020.
+ * Mubby inc
+ * mubarack.tahirr@gmail.com
+ */
+object Constants {
+    const val ONE_SECOND = 1000L
+}
