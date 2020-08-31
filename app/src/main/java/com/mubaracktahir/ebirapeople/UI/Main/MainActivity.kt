85bdb49e218466a2024b2bd84aeab208e5a367d6
Mubarack tahir <mubarack.tahir@yahoo.com>
2020-08-31 22:14:54 +0100

    added admob in app video ads

--- app/src/main/java/com/mubaracktahir/ebirapeople/UI/Main/MainActivity.kt
+++ app/src/main/java/com/mubaracktahir/ebirapeople/UI/Main/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.mubaracktahir.ebirapeople.UI.Main
 
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.reward.RewardItem
+import com.google.android.gms.ads.reward.RewardedVideoAd
+import com.google.android.gms.ads.reward.RewardedVideoAdListener
 import com.mubaracktahir.ebirapeople.R
 import com.mubaracktahir.ebirapeople.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RewardedVideoAdListener {
     lateinit var binding: ActivityMainBinding;
     lateinit var navController: NavController
+
+    private lateinit var mRewardedVideoAd: RewardedVideoAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        MobileAds.initialize(this)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(this)
+        mRewardedVideoAd.rewardedVideoAdListener = this
+        loadRewardedVideoAd()
+        if (mRewardedVideoAd.isLoaded)
+            mRewardedVideoAd.show()
         setUpNavController()
+    }
+
+    private fun loadRewardedVideoAd() {
+        mRewardedVideoAd.loadAd(
+            "ca-app-pub-2619885239553733/6352789853",
+            AdRequest.Builder().build()
+        )
     }
 
     private fun setUpNavController() {
@@ -25,5 +46,37 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+    }
+
+    override fun onRewardedVideoAdClosed() {
+        loadRewardedVideoAd()
+    }
+
+    override fun onRewardedVideoAdLeftApplication() {
+
+    }
+
+    override fun onRewardedVideoAdLoaded() {
+
+    }
+
+    override fun onRewardedVideoAdOpened() {
+
+    }
+
+    override fun onRewardedVideoCompleted() {
+
+    }
+
+    override fun onRewarded(p0: RewardItem?) {
+
+    }
+
+    override fun onRewardedVideoStarted() {
+
+    }
+
+    override fun onRewardedVideoAdFailedToLoad(p0: Int) {
+
     }
 }
