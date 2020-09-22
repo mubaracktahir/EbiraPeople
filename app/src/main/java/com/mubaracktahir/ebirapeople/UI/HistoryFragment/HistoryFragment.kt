f5794f05d6f9ce60171801c97e1d9cb9b64fee7f
Mubarack tahir <mubarack.tahir@yahoo.com>
2020-09-22 19:55:50 +0100

    added back to history fragment

--- app/src/main/java/com/mubaracktahir/ebirapeople/UI/HistoryFragment/HistoryFragment.kt
+++ app/src/main/java/com/mubaracktahir/ebirapeople/UI/HistoryFragment/HistoryFragment.kt
@@ -1,8 +1,15 @@
 package com.mubaracktahir.ebirapeople.UI.HistoryFragment
 
+import androidx.navigation.findNavController
 import com.mubaracktahir.ebirapeople.R
 import com.mubaracktahir.ebirapeople.core.FullScreenFragment
 import com.mubaracktahir.ebirapeople.databinding.FragmentHistoryBinding
 
 
-class HistoryFragment : FullScreenFragment<FragmentHistoryBinding>(R.layout.fragment_history)
+class HistoryFragment : FullScreenFragment<FragmentHistoryBinding>(R.layout.fragment_history){
+    override fun init() {
+        binding.fabs.setOnClickListener {
+            view?.findNavController()?.popBackStack()
+        }
+    }
+}
